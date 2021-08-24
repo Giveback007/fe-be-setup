7675ed1aa7f036191ba9d266e431168352a46de9
giveback007 <dovy91@gmail.com>
2021-08-23 23:36:34 -0400

    added auto logo/icon generation for pwa

--- esbuild.build.ts
+++ esbuild.build.ts
@@ -1,12 +1,47 @@
 import esbuild = require('esbuild');
 import { remove, mkdir, copy } from 'fs-extra';
-const { join } = require('path');
+import pwaAssetGenerator from 'pwa-asset-generator';
+import { join } from 'path';
 const sassPlugin = require('esbuild-plugin-sass');
 const { log } = console;
 
+async function pwaAssets() {
+    // apple splash screens
+    await pwaAssetGenerator.generateImages(
+        "./frontend/assets/squarelogo.png",
+        "./dist/public",
+        {
+            scrape: true,
+            background: "linear-gradient(228deg, rgba(26,26,26,1) 0%, rgba(26,26,26,1) 25%, rgba(55,120,194,1) 66%)",
+            opaque: true,
+            splashOnly: true,
+            log: true,
+            manifest: 'dist/manifest.webmanifest',
+            index: 'dist/index.html'
+        }
+    );
+
+    // icons
+    await pwaAssetGenerator.generateImages(
+        "./frontend/assets/squarelogo.png",
+        "./dist/public",
+        {
+            scrape: true,
+            opaque: true,
+            padding: '0%',
+            iconOnly: true,
+            log: false,
+            manifest: 'dist/manifest.webmanifest',
+            index: 'dist/index.html',
+            favicon: true,
+            maskable: true,
+        }
+    );
+}
+
 async function build(type: 'frontend' | 'backend', toDir: string) {
     const t1 = Date.now();
-    log(`🏗️  Building ${type}... 🔨 to '${join(toDir)}'`);
+    log(`🏗️ Building ${type}... 🔨 to '${join(toDir)}'`);
 
     // ### Clean previous build files
     await remove(toDir);
@@ -14,8 +49,9 @@ async function build(type: 'frontend' | 'backend', toDir: string) {
 
     // FRONTEND //
     if (type === 'frontend') {
-        await copy('frontend/_pwa', join(toDir, 'public'));
-        await copy(join('frontend', 'index.html'), join(toDir, 'index.html'));
+        const esbuildTime = Date.now();
+        await copy('frontend/manifest.webmanifest', join(toDir, 'manifest.webmanifest'));
+        await copy('frontend/index.html', join(toDir, 'index.html'));
 
         await esbuild.build({
             target: "es2015",
@@ -40,7 +76,12 @@ async function build(type: 'frontend' | 'backend', toDir: string) {
                 '.eot': 'text',
                 '.mp3': 'file'
             }
-        })
+        });
+        log(` - [esbuild] | ${Date.now() - esbuildTime}ms`);
+
+        const logoTime = Date.now();
+        await pwaAssets();
+        log(` - [pwa-asset-generator] | ${Date.now() - logoTime}ms`);
     }
 
     // BACKEND //
@@ -56,7 +97,7 @@ async function build(type: 'frontend' | 'backend', toDir: string) {
     }
 
     const t2 = Date.now();
-    log(`⚡ Built in ${t2 - t1}ms`);
+    log(`✔️ Built in ${((t2 - t1) / 1000).toFixed(2)}s`);
 }
 
 const [type] = process.argv.slice(2);
